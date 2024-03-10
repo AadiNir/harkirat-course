3590f09adc24dfc148c5c4e81ad3ebfe68fc0112
Aadinir <aadiniranjan27@gmail.com>
2024-03-10 21:11:49 +0530

    et commit

--- React/foundofr/advreact/src/Components/Effecttrain.jsx
+++ React/foundofr/advreact/src/Components/Effecttrain.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 function Effecttrain() {
     const [data, setData] = useState("");
     const [error, setError] = useState(null);
-    const [id,setid]=useState("1");
+    const [id,setid]=useState("2");
 
     useEffect(() => {
         axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
@@ -19,8 +19,12 @@ function Effecttrain() {
 
     return (
         <div>
-            <input type='text' placeholder='enter the id of todo list u want to retrive'></input>
-            <button onSubmit={(e)=>setid(e.target.value)}>Done</button>
+            {/* <input type='text' placeholder='enter the id of todo list u want to retrive'></input> */}
+            <button onClick={(e)=>setid("1")}>1</button>
+            <button onClick={(e)=>setid("2")}>2</button>
+            <button onClick={(e)=>setid("3")}>3</button>
+            <button onClick={(e)=>setid("4")}>4</button>
+
             <h1>These are</h1>
             {error ? (
                 <p>Error: {error}</p>
