487107d51dd5bc8a3a5f7658970d3b6ebb604ab9
Aadinir <aadiniranjan27@gmail.com>
2024-03-14 21:48:13 +0530

    lazy loading

--- React/foundofr/advreact/src/App.jsx
+++ React/foundofr/advreact/src/App.jsx
@@ -1,19 +1,41 @@
-import { useState } from 'react'
+import { memo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Effecttrain from './Components/Effecttrain'
 import Memotrain from './Components/Memotrain'
 import Usereftrain from './Components/Usereftrain'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+
+const MainPage=()=>{
+  return(
+    <h1>Main page bro</h1>
+  )
+}
+const HandlePage=()=>{
+  return(
+    <h1>Handle Page bro</h1>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const handlepage=()=>{
+    window.location.href='handle'
+  }
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='/handle' element={<HandlePage/>}/>
+        </Routes>
+      </BrowserRouter>
+      <button onClick={handlepage}>Click for route</button>
       {/* <Effecttrain/> */}
-      {/* <Memotrain/> */}
-      <Usereftrain/>
+      <Memotrain/>
+      {/* <Usereftrain/> */}
     </>
   )
 }
--- React/foundofr/advreact/src/Components/Memotrain.jsx
+++ React/foundofr/advreact/src/Components/Memotrain.jsx
@@ -2,39 +2,23 @@ import React, { useEffect } from 'react'
 import { memo,useMemo,useState,useCallback } from 'react'
 
 function Memotrain() {
-  const [id,setid]= new useState(Number(0));
-  const [bank1,setbank1] = new useState(Number(0));
-  const [bank2,setbank2] =new useState(Number(0));
-  
-  // const a = useCallback(function(){
-  //   return(
-  //     <h1>Hey there</h1>
-  //   )
-  // },[])
-  useEffect(()=>{
-    setbank1(30);
-    console.log("cred1");
-  },[])
-  useEffect( ()=>{
-    setbank2(300);
-    console.log("cred2");
-  },[])
-
-  useEffect(()=>{
-    console.log("end credit");
-  },[id])
-
-  const g = useMemo(()=>{
-     setbank2(bank1+bank2);
-    return bank2;
-  },[])
-
+  const [fact,setfact]= new useState(Number(0)); 
+  const [ans,setans] = new useState(Number(0));
+  const factca = useMemo(()=>{
+    console.log("hey");
+    let k =1;
+    for(let i=1;i<fact;i++){
+        k=k*i;
+    }
+    setans(k);
+  },[fact])
   return (
     <div>
     Memotrain
-    <button onClick={()=>{setid(id+1)}}>the value{id}</button>
-    {/* <Demo a={id} /> */}
-    <h1>{g}</h1>
+    <input onChange={(e)=>setfact(e.target.value)} placeholder='enter the input factorial'/>
+    <h1>{ans}</h1>
+    <input onChange={(e)=>setans(e.target.value)} placeholder='enter the input factorial'/>
+
     </div>
   )
 }
