bd17ab0735e000dcf3fe794c6ce1e9624ec1735d
Aadinir <aadiniranjan27@gmail.com>
2024-03-17 16:43:30 +0530

    backend added

--- Paytmproject/Backend/Routes/index.js
+++ Paytmproject/Backend/Routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const User = require('./user')
 const Mainrouter = express.Router();
+
+Mainrouter.use('/user',User)
 module.exports = Mainrouter;
