e8703246075a56f3edfd00e5f3013ce89c713f34
Aadinir <aadiniranjan27@gmail.com>
2024-04-17 20:09:06 +0530

    new commit

--- Paytmproject/Backend/Routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-const User = require('./user')
-const Mainrouter = express.Router();
-
-Mainrouter.use('/user',User)
-module.exports = Mainrouter;
--- Paytmproject/Backend/db.js
+++ Paytmproject/Backend/db.js
@@ -1,13 +1,13 @@
-//  function User(){
- const mongoose = require('mongoose');
-//  mongoose.connect("mongodb+srv://aadinir:Aamod123@cluster0.sszhzlo.mongodb.net/?retryWrites=true&w=majority")
- const User = mongoose.model("User",{
-    Username:String,
-    FirstName:String,
-    LastName:String,
-    Password:String 
- })
-// }
-module.exports={
-    User
-};
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://aadinir:Aamod123@cluster0.sszhzlo.mongodb.net/")
+const usrschema = new mongoose.model('User-schema',new Schema(
+    {
+        username:String,
+        password:String,
+        firstname:String,
+        lastname:String
+    }
+));
+module.exports = {
+    usrschema
+}
