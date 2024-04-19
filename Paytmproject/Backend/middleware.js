d92b4422d9ae36d6e4bf06a0a0b962390fc4358e
Aadinir <aadiniranjan27@gmail.com>
2024-04-19 22:00:58 +0530

    new commit

---
+++ Paytmproject/Backend/middleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('./config');
+function authMiddleware(req,res,next){
+    const gottoken = req.header.Authorization;
+    if(!gottoken || gottoken.startswith("Bearer ")){
+        return res.status(403).json({});
+    }
+    const ggtoken = gottoken.split(" ")[1];
+    try{
+        const decoded = jwt.verify(ggtoken,JWT_SECRET);
+        req.userId = decoded.usrid;
+    }catch(err){
+    return res.status(403);
+    }
+}
+moudule.exports = authMiddleware;
