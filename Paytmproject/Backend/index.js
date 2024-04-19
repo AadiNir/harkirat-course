57f724c2845c91a43458a3228aa6e09883c98d71
Aadinir <aadiniranjan27@gmail.com>
2024-04-19 20:50:02 +0530

    new changes

---
+++ Paytmproject/Backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = require('../api/index');
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1',router);
