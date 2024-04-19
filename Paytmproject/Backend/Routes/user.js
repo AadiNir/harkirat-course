557e1794592c62b8926a4862db67a6a6c2227ddc
Aadinir <aadiniranjan27@gmail.com>
2024-04-19 21:33:22 +0530

    new commit

--- Paytmproject/Backend/Routes/user.js
+++ Paytmproject/Backend/Routes/user.js
@@ -20,7 +20,7 @@ router.post('/signup',async (req,res)=>{
         }
         )
     }
-    const userexsist = await User.findOne(data.username);
+    const userexsist = await User.findOne({username:data.username});
     if(userexsist){
         res.status(411).json({
             message:"Email already taken/Incorrect inputs"
@@ -33,5 +33,37 @@ router.post('/signup',async (req,res)=>{
         lastname: data.lastnae
     })
     const token = jwt.sign(usr._id,JWT_SECRET);
+
+    res.json({
+        message:"user created successfully",
+        Token: token
+    })
+})
+const usrcheck = z.object({
+    username:z.string(),
+    password:z.string()
+})
+router.post('signin',async (req,res)=>{
+     const data = req.body;
+     try{
+        usrcheck.parse(data);
+     }catch(err){
+        res.status(411).json({
+            message:"Sign in unsuccessfull"
+        })
+     }
+     const usr = await User.findone({
+        username:data.username,
+        password:data.password
+    });
+    if(usr){
+        const token = jwt.sign(usr._id,JWT_SECRET);
+        res.json({
+            Token: token
+        })
+     }
+     res.json({
+        message:"Sign in unsuccessfully"
+     })
 })
 module.exports = router;
