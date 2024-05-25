d5abe9e01fe45ff5b8993663b2f2881cedf2721c
Aadinir <aadiniranjan27@gmail.com>
2024-05-26 00:30:24 +0530

    new changes

--- Paytmproject/paytmproj/src/Pages/Signin.jsx
+++ Paytmproject/paytmproj/src/Pages/Signin.jsx
@@ -6,7 +6,7 @@ function Signin() {
     <div className='flex flex-row items-center justify-center min-h-screen'>
  
     <div className='bg-slate-600 p-20 rounded-lg'>
-    <div className='flex flex-col items-center '>
+    <div className='flex flex-col items-center '> ad
         <Heading label={"Sign in"}/>
         <Inputbox placehold={"Name"}  />
         <Inputbox placehold={"LastName"}  />
